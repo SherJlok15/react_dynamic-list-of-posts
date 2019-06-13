7a7f487e4464d7e14af058a21680094277a94722
Fandorin <SherJlok15@gmail.com>
2019-06-13 14:11:56 +0300

    Updates

---
+++ precache-manifest.79b3c674f8cd40cea1e420322cd034d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d0d81917ff494719be6e6eef1bcd0d",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "5bb0f3cd3d9194055913",
+    "url": "/react_dynamic-list-of-posts/static/css/main.adcb0156.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/react_dynamic-list-of-posts/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "5bb0f3cd3d9194055913",
+    "url": "/react_dynamic-list-of-posts/static/js/main.099e34b4.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
